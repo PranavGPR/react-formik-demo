e295328a5ec3dafbdc6bab9adb03b820506cc233
Pranav <pranav.gnanasekar23@gmail.com>
2020-08-22 11:37:23 +0530

    form style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import YoutubeForm from "./components/YoutubeForm"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <YoutubeForm />
     </div>
   );
 }
