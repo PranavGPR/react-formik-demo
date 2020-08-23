870a913124d5ca4d88e20bb2646547caaf8a76a2
Pranav <pranav.gnanasekar23@gmail.com>
2020-08-23 11:09:09 +0530

    validation add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import YoutubeForm from "./components/YoutubeForm"
 import './App.css';
 
--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -39,19 +39,26 @@ function YoutubeForm() {
     validate
   })
 
-  // console.log("form values", formik.values);
-
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" onChange={formik.handleChange} value={formik.values.name} />
+          {formik.errors.name ? <div className="error">{formik.errors.name}</div> : null}
+        </div>
 
-        <label htmlFor="email">E-mail</label>
-        <input type="text" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input type="text" id="email" name="email" onChange={formik.handleChange} value={formik.values.email} />
+          {formik.errors.email ? <div clasnnaclassName="error">{formik.errors.email}</div> : null}
+        </div>
 
-        <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <input type="text" id="channel" name="channel" onChange={formik.handleChange} value={formik.values.channel} />
+          {formik.errors.channel ? <div clasnnaclassName="error">{formik.errors.channel}</div> : null}
+        </div>
 
         <button type="submit">Submit</button>
       </form>
